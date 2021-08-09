3d690d4e3fd9205cbf15a2e82a63820d296a9b3b
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-08-08 20:01:40 -0500

    Update script.js

--- staff/shonna/js/script.js
+++ staff/shonna/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   
   $('rounded').click(function(){
-    $('div').css({'border-radius','10em'});
+    $('div').css({'border-radius':'10em'});
   });
   
   /*$("#rounded").click(function(){
