5291326c9cb0ecbc8e39748de55c739b60000db3
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-08-08 20:09:38 -0500

    Update script.js

--- staff/shonna/js/script.js
+++ staff/shonna/js/script.js
@@ -4,15 +4,11 @@ $(document).ready(function() {
     $('div').css({'border-radius':'10em'});
   });
   
-  /*$("#rounded").click(function(){
-    $("div").css({"border-radius","10em"});
-  });
-  
   $("p").click(function(){
-    $("p").css({"background-color","blue"});
+     alert("A paragraph was clicked.");
   });
   
-  $("#change-text").click(function(){
+  /*$("#change-text").click(function(){
     $("#change-text").css({"color","green"});
   });
   
