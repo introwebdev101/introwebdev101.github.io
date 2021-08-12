0777054a6b30f38b9ee052c375a5c257b7f75870
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-08-12 12:12:44 -0500

    Update script.js

--- staff/elizabeth/js/script.js
+++ staff/elizabeth/js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
+$("#rounded").click(function(){
+  $("div").css({"border-radius":"10em"});
+});
+  $("p").click(fuction(){
+               alert("A paragraph was click.");
+});
 
+$("#change").click(function(){
+  $("p").css({"color":"green"});
+});
+
+  
+  
 });
