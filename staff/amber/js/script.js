1d85ce80b36e60bc00b356aa0dd62c77432d3d12
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-08-12 12:19:28 -0500

    Update script.js

--- staff/amber/js/script.js
+++ staff/amber/js/script.js
@@ -2,4 +2,11 @@ $(document).ready(function() {
  $("#change").click(function(){
    $("p").css({"color":"orange"});
  });
+ $("#rounded").click(function(){
+  $("div").css({"border-radius":"12em"});
+ });
+ $("p").click(function(){
+  alert("A paragraph was clicked.");
+ });
+ 
 });
