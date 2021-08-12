3ff08099c8adbe115e79be3b200369122c9fbaaf
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-08-12 12:28:31 -0500

    Update script.js

--- staff/elizabeth/js/script.js
+++ staff/elizabeth/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
   $("#rounded").click(function(){
-  $("div").css({"border-radius":"10em"});
+    $("div").css({"border-radius":"10em"});
 });
-  $("p").click(fuction(){
+  $("p").click(function(){
                alert("A paragraph was clicked.");
 });
 
