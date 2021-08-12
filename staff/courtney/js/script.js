010ef40a2a6f7387c70c0081cd13f5c06311dcad
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-08-12 12:19:37 -0500

    Update script.js

--- staff/courtney/js/script.js
+++ staff/courtney/js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
  $("#change").click(function(){
   $("p").css({"color":"green"});
-  });
+ });
+ 
+ $("#rounded").click(function(){
+  $("div").css({"border-radius":"10em"});
 });
+ 
+ 
+});
+
