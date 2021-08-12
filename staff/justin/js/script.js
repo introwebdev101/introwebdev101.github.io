ae3bc8cd9bdce5dfc217699a5d961b35f9dc62d3
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-08-12 12:23:10 -0500

    Update script.js

--- staff/justin/js/script.js
+++ staff/justin/js/script.js
@@ -2,12 +2,13 @@ $(document).ready(function() {
 $("#change").click(function(){
   $("p").css({"color":"green"});
              });
-  
   $("rounded").click(function(){
     $("div").css({"border-radius":10em"});
-                  });
+    });
  $("p").click(function(){
       alert("Oh, no.");
    });
+    $('div').click(function(){
+      $(this).hide();
+    });
 });
-
