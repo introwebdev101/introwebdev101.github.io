ad25f887439ff37fd9d7d21d4a8cbc7d95045025
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-08-12 12:12:12 -0500

    Update script.js

--- staff/justin/js/script.js
+++ staff/justin/js/script.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
 
-$({ class: 'feather-icon', 'stroke-width': 2 })
-
 $("#change").click(function(){
-  $("p").css({"color":"yellow"}];
+  $("p").css({"color":"blue"}];
              });
 });
 
