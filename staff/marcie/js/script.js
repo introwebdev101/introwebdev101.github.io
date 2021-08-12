513e9083d6c625bb4a7048d4e6104c6708040900
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-08-12 12:23:35 -0500

    Update script.js

--- staff/marcie/js/script.js
+++ staff/marcie/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("#change").click(function(){
-    $("p").css({"color":#9370DB});
+    $("p").css({"color":"#9370DB"});
   });
   $("#rounded").click(function(){
     $("div").css({"border-radius":"10em"});
