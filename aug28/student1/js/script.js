e5bae33de90703510fdd96102f73b12e03cf1f5d
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-08-28 12:30:25 -0500

    Update script.js

--- aug28/student1/js/script.js
+++ aug28/student1/js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-
+$("#rounded").click(function(){
+  $("div".css({"border-radius":10em"});
+  });
+ 
+$("p").click(function(){
+    alert("A paragraph was clicked.");
+  });
+  
+$("#change").click(function(){
+  $("p").css({"color":"pink"});
+});
+  
 });
