12468f6b6a37d46d1a9d8d43f608571b58419db7
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-09-17 10:59:58 -0500

    Update script.js

--- oct2/jodi/js/script.js
+++ oct2/jodi/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 $("#rounded").click(function(){
   $("div").css({"border-radius":"10em"});
-  });
+});
   
   $("p").click(function(){
     alert("A paragraph was clicked.");
@@ -11,7 +11,7 @@ $("#rounded").click(function(){
     $("p").css({"color":"purple"});
   });
   
-  $('div').click(function(){
+  $("div").click(function(){
     $(this).hide();
   });
   
