b78439e2232d6bb6412ccf9b1b6002d7b247af16
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-09-17 10:53:27 -0500

    Update script.js

--- oct2/mike/js/script.js
+++ oct2/mike/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $("#rounded").click(function(){
-    $("div".css({border-radius":"10em"});
+    $("div".css({"border-radius":"10em"});
 });
-  $("P").click(function(){
+  $("p").click(function(){
 alert("A paragpraph was clicked.");
 });
- $("#change").click(function();{
+ $("#change").click(function(){
     $("p").css({"color":"#ccff33"});
 });
  $('div').click(function(){
