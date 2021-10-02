b91b27152b968c36bf287c7c0a285e00233eb5ff
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-10-02 12:23:42 -0500

    Update script.js

--- oct2/elie/js/script.js
+++ oct2/elie/js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
-$('div').click(function(){
-  $(this).hide();
+$("#rounded").click(function(){
+  $("div").css({"border-radius":"10em"});
+});
+  $)"p").click(function(){
+    alert("A paragraph was clicked.");
+  });
+  $("#change").click(function(){
+    $("p").css({"color":"green"});
+  });
+  $("#hide").click(function(){
+    $("img").toggle():
+  });
+  $("div").hide();
+});
 });
