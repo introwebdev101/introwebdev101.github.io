c9a6dca417d99710301f3bfc6f9567065cd0b537
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-10-02 09:35:30 -0700

    Update script.js

--- oct2/spencer/js/script.js
+++ oct2/spencer/js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-
+  $("#rounded").click(function(){
+  $("div").css({"border-radius":"10em"});
+});
+$("p").click(function(){
+  alert("A paragraph was clicked, is this thing on, word?");
+});
+                              $("#change").click(function(){
+                                $("p").css({"color":"green"});
+                              });
+                              $("#hide").click(function(){
+                                $("img").toggle();
+                              });
+                               $('div').click(function(){
+                                $(this).hide();
+                               });
 });
