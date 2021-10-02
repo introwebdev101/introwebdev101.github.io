53eaf273a2a9e75a61de996266b15b26d3f972a3
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-10-02 12:34:52 -0500

    Update script.js

--- oct2/monica/js/script.js
+++ oct2/monica/js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-$("#rounded").click(funtion(){
+$("#rounded").click(function(){
   $("div").css({"border-radius":"10em"});
 });
+
 $("p").click(function(){
   alert("A paragraph was clicked.");
 });
