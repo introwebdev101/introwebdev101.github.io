795ab6593c6ad106268eb1472db88c9ced77d13e
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-10-02 12:42:05 -0500

    Update script.js

--- oct2/nishat/js/script.js
+++ oct2/nishat/js/script.js
@@ -1,27 +1,25 @@
-$(document).ready(function(){
+$(document).ready(function() {
 $("#rounded").click(function(){
- $("div").css({"border-radius"."10em"});
+  $("div").css({"border-radius":"10em"});
 });
- 
+  
   $("p").click(function(){
-   alert("A paragraph was clicked.");
- });
- 
+    alert("A paragraph was clicked.");
+  });
+  
   $("#change").click(function(){
     $("p").css({"color":"green"});
   });
- 
-  $("#hide").click(function(){
-    $("img").toggle();
-  });
- 
- $("div").click(function(){
+  
+  $("div").click(function(){
     $(this).hide();
   });
   
- 
+  $("#hide").click(function(){
+    $("img").toggle();
+  });
+
 });
-  
 
 
 
