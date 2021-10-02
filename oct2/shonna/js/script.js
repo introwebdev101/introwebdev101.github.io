3cca246b6c48941b6f222b514e0f336e62a1227e
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-10-02 12:21:58 -0500

    Update script.js

--- oct2/shonna/js/script.js
+++ oct2/shonna/js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
+$("#rounded").click(function(){
+  $("div").css({"border-radius":"10em"});
+});
+  
+  $("p").click(function(){
+    alert("A paragraph was clicked.");
+  });
+  
+  $("#change").click(function(){
+    $("p").css({"color":"green"});
+  });
+  
+  $("#hide").click(function(){
+    $("img").toggle();
+  });
 
 });
