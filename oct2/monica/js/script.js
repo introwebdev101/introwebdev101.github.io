2aafa84d7d066340ac052178b8887e045d291306
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-10-02 12:25:23 -0500

    Update script.js

--- oct2/monica/js/script.js
+++ oct2/monica/js/script.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
+$("#rounded").click(funtion(){
+  $("div").css({"border-radius":"10em"});
+});
+$("p).click(function(){
+  alert("A paragraph was clicked.");
+});
+
+$("#change").click(funtion(){
+ $("p").css({"color":"green"});
+});
+
+$('div').click(function(){
+  $(this).hide();
+});
+
+$("#hide").click(function(){
+  $("img").toggle();
+});
+  
 
 });
