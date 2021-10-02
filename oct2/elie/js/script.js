ac3b80674cabc5f4e10312f88067c9eaf45c5bfb
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-10-02 12:25:32 -0500

    Update script.js

--- oct2/elie/js/script.js
+++ oct2/elie/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 $("#rounded").click(function(){
   $("div").css({"border-radius":"10em"});
 });
-  $)"p").click(function(){
+  $("p").click(function(){
     alert("A paragraph was clicked.");
   });
   $("#change").click(function(){
