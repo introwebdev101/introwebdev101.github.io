ec070bb99bc65f04423566b8f6f77abb1d5859dd
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-10-11 19:36:04 -0500

    Update script.js

--- stem/student3/js/script.js
+++ stem/student3/js/script.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
+  
 $("#rounded").click(function(){
   $("div").css({"border-radius":"10em"});
+  });
+  
   $("p").click(function(){
     alert("A paragraph was clicked.");
   });
+  
   $("#change").click(function(){
     $("p").css({"color":"green"});
   });
+  
   $("div").click(function(){
     $(this).hide();
   });
