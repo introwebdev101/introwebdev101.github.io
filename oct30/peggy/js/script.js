61ac66f5d58a311636d53c5031be202411346828
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2021-10-30 12:41:15 -0500

    Update script.js

--- oct30/peggy/js/script.js
+++ oct30/peggy/js/script.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-
+  $("#rounded").click(function(){
+  $("div").css({"border-radius":"10em"});
+});
+  
+  $("p").click(function(){
+alert("A paragraph was clicked.");
+});
+  
+  $("#change").click(function(){
+  $("p").css({"color":"green"});
+});
+  
+  $("#hide").click(function(){
+  $("img").toggle();
+});
+  
+  $("div").click(function(){
+  $(this).hide();
+  });
 });
