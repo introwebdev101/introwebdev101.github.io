40a137fdf1a0388a49c9b2b85d3d2b2d21d89a14
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2022-03-08 15:39:04 -0600

    Create script.js

---
+++ restore/march/shonna/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $("img").fadeIn(1000);
+  
+  $("img").click(function(){
+    $("img").css({"border-radius":"10em"});
+  });
+  
+  $("body").click(function(){
+    alert("You clicked somewhere on the page!");
+  });
+  
+  $("btn1").click(function(){
+    $("img").css({"border-radius":"0px"});
+  });
+  
+  $("btn2").click(function(){
+    $("img").toggle();
+  });
+  
+ )};
