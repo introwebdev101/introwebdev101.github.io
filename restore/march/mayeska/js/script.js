1a46041b91039614d037ce1809d059389a734724
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2022-03-08 19:07:37 -0600

    Update script.js

--- restore/march/mayeska/js/script.js
+++ restore/march/mayeska/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   });
   
     $("img").click(function(){
-      $("img").css({"border-radius":"10em"});
+      $("img").css("border-radius":"10em");
     });
   
 });
