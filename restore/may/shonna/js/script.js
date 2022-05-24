78fcb0525daf2f2e837d692aa43a2bfd80be52c1
introwebdev101 <88550360+introwebdev101@users.noreply.github.com>
2022-05-24 18:59:19 -0500

    Update script.js

--- restore/may/shonna/js/script.js
+++ restore/may/shonna/js/script.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
 
   
-  $("button").click(function(){
-    $("img").toggle();
+  $("img").click(function(){
+    $(this).hide();
   });
-   $("img").click(function(){
-      $("img").css("border-radius","100px");
-    });  
    
 });
